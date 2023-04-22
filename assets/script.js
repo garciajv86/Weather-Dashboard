e9ac1978b115228750cb3c96fbe9becd1ad57607
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-04-21 23:26:08 -0500

    added cordinates function

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,11 @@ var weatherForecastApi = 'https://api.openweathermap.org/data/2.5/forecast?lat={
 
 var searchBtn = document.getElementById('search-button');
 
-
-searchBtn.addEventListener('click', function(event) {
-
-    event.preventDefault();
-
+function getCoordinates()
+ {
     var  city = document.getElementById('city-search').value;
+    localStorage.setItem('City', city);
+    
     // Geo Code API
     var geoCodingApi = "http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=20&appid=a7fc7c3921309a588e45475792082481";
 
@@ -20,10 +19,25 @@ searchBtn.addEventListener('click', function(event) {
         return response.json();
     })
     .then(function(data) {
-        console.log(data[0].lon);
-        console.log(data[0].lat);
+        // console.log(data[0].lon);
+        // console.log(data[0].lat);
+
+        const latLon = {
+            lat: data[0].lon,
+            lon: data[0].lat
+        }
+        console.log(latLon);
+
+        localStorage.setItem('coordinate',JSON.stringify(latLon));
 
     })
+};
+
+
+searchBtn.addEventListener('click', function(event) {
+
+    event.preventDefault();
+    getCoordinates();
 
 })
 
